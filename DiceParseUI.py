819a6e67eadd023df6ee03e7752909a5631ac346
bushfacts <bushfactsanalytics@gmail.com>
2025-08-28 15:25:54 -0500

    fully functioning for viewing results. Still need to implement creating dataset

--- DiceParseUI.py
+++ DiceParseUI.py
@@ -8,30 +8,118 @@ from PyQt6.QtGui import QFont
 
 from Functions import *
 
+BLUEPLAYER = "Brian Baer"
+REDPLAYER = "BushFacts"
 
+##################################################
+# FUNCTIONS
+##################################################
 
+# these Get functions are inefficient by choice for robustitudeness
 def NextOffense():
+    global offenseIndex
     offenseIndex = offenseIndex + 1
+    dieRoll = GetOffense()
+    UpdateOffense(dieRoll)
 
 def PrevOffense():
+    global offenseIndex
     offenseIndex = offenseIndex - 1
+    dieRoll = GetOffense()
+    UpdateOffense(dieRoll)
+
+def GetOffense():
+    global offenseIndex
+    count = 0
+    for i in range(0,len(diceData)):
+        if diceData[i]["Offense"]:
+            if count == offenseIndex:
+                print(diceData[i])
+                return diceData[i]
+            count = count + 1
+
+def UpdateOffense(dieRoll):
+    global timeO
+    global nameO
+    global redOInput
+    global blackOInput
+    global whiteOInput
+    global offenseDictResult
+    timeO.setText(dieRoll['Time'])
+    nameO.setText(BLUEPLAYER if dieRoll['Blue'] else REDPLAYER)
+    redOInput.setText(str(dieRoll['Pool']['Red']))
+    blackOInput.setText(str(dieRoll['Pool']['Black']))
+    whiteOInput.setText(str(dieRoll['Pool']['White']))
+    offenseDictResult = dieRoll['Results']
+    UpdateOffenseResults()
+
+def UpdateOffenseResults():
+    global surgeOInput
+    global resultOInput
+    global offenseDictResult
+    if surgeOInput.isChecked():
+        resultOInput.setText(str(offenseDictResult['Crits'] + offenseDictResult['Hits'] + offenseDictResult['Surges']))
+    else:
+        resultOInput.setText(str(offenseDictResult['Crits'] + offenseDictResult['Hits']))
 
 def NextDefense():
+    global defenseIndex
     defenseIndex = defenseIndex + 1
+    dieRoll = GetDefense()
+    UpdateDefense(dieRoll)
 
 def PrevDefense():
+    global defenseIndex
     defenseIndex = defenseIndex - 1
+    dieRoll = GetDefense()
+    UpdateDefense(dieRoll)
+
+def GetDefense():
+    global defenseIndex
+    count = 0
+    for i in range(0,len(diceData)):
+        if not diceData[i]["Offense"]:
+            if count == defenseIndex:
+                print(diceData[i])
+                return diceData[i]
+            count = count + 1
+
+def UpdateDefense(dieRoll):
+    global timeD
+    global nameD
+    global redDInput
+    global whiteDInput
+    global defenseDictResult
+    timeD.setText(dieRoll['Time'])
+    nameD.setText(BLUEPLAYER if dieRoll['Blue'] else REDPLAYER)
+    redDInput.setText(str(dieRoll['Pool']['Red']))
+    whiteDInput.setText(str(dieRoll['Pool']['White']))
+    defenseDictResult = dieRoll['Results']
+    UpdateDefenseResults()
+    
+def UpdateDefenseResults():
+    global surgeDInput
+    global resultDInput
+    global defenseDictResult
+    if surgeDInput.isChecked():
+        resultDInput.setText(str(defenseDictResult['Blocks'] + defenseDictResult['Surges']))
+    else:
+        resultDInput.setText(str(defenseDictResult['Blocks']))
 
 def Submit():
     NextOffense()
     NextDefense()
 
 def Done():
+    global offenseIndex
+    global defenseIndex
     print(offenseIndex, defenseIndex)
 
 
 
-
+##################################################
+# GUI
+##################################################
 
 app = QApplication(sys.argv)
 widget = QWidget()
@@ -41,7 +129,9 @@ window.setWindowTitle("Dice Parse Helper")
 window.setMinimumSize(250,250)
 
 offenseIndex = 0
+offenseDictResult = {}
 defenseIndex = 0
+defenseDictResult = {}
 mainIndex = 0
 
 filePath = "chat_log.txt"
@@ -76,12 +166,16 @@ blackO = QLabel("Black")
 mainLayout.addWidget(blackO, 4, 0, 1, 1, Qt.AlignmentFlag.AlignRight)
 whiteO = QLabel("White")
 mainLayout.addWidget(whiteO, 5, 0, 1, 1, Qt.AlignmentFlag.AlignRight)
+resultO = QLabel("Result")
+mainLayout.addWidget(resultO, 6, 0, 1, 1, Qt.AlignmentFlag.AlignRight)
 surgeO = QLabel("Surge")
 mainLayout.addWidget(surgeO, 2, 0, 1, 1, Qt.AlignmentFlag.AlignRight)
 redD = QLabel("Red")
 mainLayout.addWidget(redD, 3, 2, 1, 1, Qt.AlignmentFlag.AlignRight)
 whiteD = QLabel("White")
 mainLayout.addWidget(whiteD, 5, 2, 1, 1, Qt.AlignmentFlag.AlignRight)
+resultD = QLabel("Result")
+mainLayout.addWidget(resultD, 6, 2, 1, 1, Qt.AlignmentFlag.AlignRight)
 surgeD = QLabel("Surge")
 mainLayout.addWidget(surgeD, 2, 2, 1, 1, Qt.AlignmentFlag.AlignRight)
 #endregion
@@ -105,38 +199,44 @@ blackOInput = QLineEdit()
 mainLayout.addWidget(blackOInput, 4, 1, 1, 1)
 whiteOInput = QLineEdit()
 mainLayout.addWidget(whiteOInput, 5, 1, 1, 1)
+resultOInput = QLineEdit()
+mainLayout.addWidget(resultOInput, 6, 1, 1, 1)
 surgeOInput = QCheckBox()
+surgeOInput.stateChanged.connect(UpdateOffenseResults)
 mainLayout.addWidget(surgeOInput, 2, 1, 1, 1)
 redDInput = QLineEdit()
 mainLayout.addWidget(redDInput, 3, 3, 1, 1)
 whiteDInput = QLineEdit()
 mainLayout.addWidget(whiteDInput, 5, 3, 1, 1)
+resultDInput = QLineEdit()
+mainLayout.addWidget(resultDInput, 6, 3, 1, 1)
 surgeDInput = QCheckBox()
+surgeDInput.stateChanged.connect(UpdateDefenseResults)
 mainLayout.addWidget(surgeDInput, 2, 3, 1, 1)
 #endregion
 
 #region Buttons
-prevButtonL = QPushButton()
+prevButtonL = QPushButton("Prev")
 prevButtonL.clicked.connect(PrevOffense)
 mainLayout.addWidget(prevButtonL, 7, 0, 1, 1)
 
-nextButtonL = QPushButton()
+nextButtonL = QPushButton("Next")
 nextButtonL.clicked.connect(NextOffense)
 mainLayout.addWidget(nextButtonL, 7, 1, 1, 1)
 
-prevButtonR = QPushButton()
+prevButtonR = QPushButton("Prev")
 prevButtonR.clicked.connect(PrevDefense)
 mainLayout.addWidget(prevButtonR, 7, 2, 1, 1)
 
-nextButtonR = QPushButton()
+nextButtonR = QPushButton("Next")
 nextButtonR.clicked.connect(NextDefense)
 mainLayout.addWidget(nextButtonR, 7, 3, 1, 1)
 
-submitButton = QPushButton()
+submitButton = QPushButton("Submit")
 submitButton.clicked.connect(Submit)
 mainLayout.addWidget(submitButton, 8, 0, 1, 4)
 
-doneButton = QPushButton()
+doneButton = QPushButton("Done")
 doneButton.clicked.connect(Done)
 mainLayout.addWidget(doneButton, 9, 0, 1, 4)
 #endregion
