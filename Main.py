295b9ab021bc0e7bfbdd40f186dafa7975b3b50c
bushfacts <bushfactsanalytics@gmail.com>
2025-09-01 07:44:23 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,13 +1,35 @@
 from Functions import *
 from ComplexBinomial import *
 import pathlib
+import json
 
 currentPath = pathlib.Path(__file__).parent.resolve()
-dataPath = currentPath.joinpath("Data\\Baer v BushFacts\\")
 timeStart = "[08:34:04]"
 
-ScrapeChatLog(dataPath.joinPath("chat_log.txt"), timeStart)
+DATAPATH = currentPath.joinpath("Data\\Baer v BushFacts\\")
+BLUE = "Brian Baer"
+RED = "BushFacts"
 
-data = GetData(dataPath)
-luckData = CalculateAttackProbabilities(data)
-print(luckData)
+RED_COLOR_PRIMARY = "salmon"
+RED_COLOR_SECONDARY = "darkred"
+BLUE_COLOR_PRIMARY = "steelblue"
+BLUE_COLOR_SECONDARY = "darkblue"
+BASE_COLOR = "lightgray"
+
+# ScrapeChatLog(dataPath.joinPath("chat_log.txt"), timeStart)
+# data = GetData(dataPath)
+
+
+def ApplyProbabilities():
+    attackData = []
+    with open(DATAPATH.joinpath("attack_rolls.json"), "r") as file:
+        attackData = json.load(file)
+    for data in attackData:
+        luckData = CalculateDiceProbability(data)
+        if "Offense" in luckData:
+            data["Offense"]["Probability"] = luckData["Offense"]
+        if "Defense" in luckData:
+            data["Defense"]["Probability"] = luckData["Defense"]
+    with open(DATAPATH.joinpath("attack_rolls.json"), "w") as file:
+        json.dump(attackData, file, indent=3)
+
