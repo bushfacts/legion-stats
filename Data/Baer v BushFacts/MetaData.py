4945d8fb271d7db3b7fa2e9d5c20a1f32e3c1a79
bushfacts <bushfactsanalytics@gmail.com>
2025-09-11 17:23:33 -0500

    Add files via upload

---
+++ Data/Baer v BushFacts/MetaData.py
@@ -0,0 +1,8 @@
+TIME_START = "[08:34:04]"
+
+BLUE = "Brian Baer"
+RED = "BushFacts"
+
+BLUE_UNITS = ["Vader", "Imperial Agent", "Stormtrooper Riot Squad_1", "Stormtrooper Riot Squad_2", "Snowtrooper_1", "Black Sun Enforcers_2", "Black Sun Enforcer_2",
+              "Dewback Rider_1", "Dewback Rider_2", "Scout Troopers_1", "Scout Troopers_2"]
+RED_UNITS = ["Yoda", "Rex", "Bad Batch", "Clone Trooper Infantry_1", "Clone Trooper Infantry_2", "Clone Trooper Infantry_3", "Commandos_1", "Commandos_2", "Commandos_3"]
