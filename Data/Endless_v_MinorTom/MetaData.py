2837b0db3d91ea6237afe602d3affb0e2be13255
bushfacts <bushfactsanalytics@gmail.com>
2025-09-11 17:22:59 -0500

    Add files via upload

---
+++ Data/Endless_v_MinorTom/MetaData.py
@@ -0,0 +1,9 @@
+TIME_START = "[08:45:42]"
+
+BLUE = "Endless"
+RED = "Minor Tom"
+
+BLUE_UNITS = ["Maul", "Black Sun Enforcers_2", "Black Sun Enforcers_3", "Black Sun Enforcers_4", "Black Sun Enforcers_5", "Black Sun Enforcers_6", "Black Sun Enforcers_7",
+              "Mandalorian Super Commandos_8", "Mandalorian Super Commandos_9"]
+RED_UNITS = ["Rebel Officer_1", "Rebel Officer_2", "Ahsoka Tano", "Rebel Agent", "Fleet Troopers_5", "Fleet Troopers_6", "Fleet Troopers_7", "Fleet Troopers_8", "Fleet Troopers_9",
+             "Fleet Troopers_10", "Rebel Sleeper Cell_1", "Rebel Sleeper Cell_2", "Rebel Sleeper Cell_3"]
