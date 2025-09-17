63000387a1975e74b4f01e9f093c6a8fb04f864f
bushfacts <bushfactsanalytics@gmail.com>
2025-09-17 17:45:11 -0500

    Create Main.py

---
+++ Army Evaluator/Main.py
@@ -0,0 +1,37 @@
+# What Numbers to describe an army?
+# Offense
+# -Power
+# ---r0
+# ---r1
+# ---r2
+# ---r3
+# ---r4
+# ---r5
+# -Quality
+# ---impact
+# ---aims
+# ---pierce
+# ---sharpshooter/blast
+# ---High Velocity
+# Defense
+# -Health
+# ---Raw HP
+# ---Effective HP
+# ---something about health/model (attrition resistance indicator)
+# -Tech
+# ---dodges (indpendent > cache > agile) nimble multiplies, outmanuever less so
+# ---armor
+# ---low profile
+# ---cover
+# Objective Winning
+# ---Force Push
+# -Free Moves
+# ---speeder
+# ---relentless/steady/charge
+# ---burst of speed/imperial march/spur
+# ---speed 3
+# ---speed 1
+# ---prepared positions/infiltrate
+
+
+# quality measures need (?) to be scaled based on the dice/bodies behind them
